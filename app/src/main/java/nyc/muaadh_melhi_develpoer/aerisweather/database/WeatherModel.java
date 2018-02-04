d44ad29ec778b00dbd864f054db90347131f3818
slinny <siranli411@gmail.com>
2018-02-04 17:54:57 -0500

    added location service and some classes for the database

---
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/aerisweather/database/WeatherModel.java
@@ -0,0 +1,36 @@
+package nyc.muaadh_melhi_develpoer.aerisweather.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+/**
+ * Created by C4Q on 2/4/18.
+ */
+
+@Entity(tableName = "weather")
+public class WeatherModel {
+
+    @PrimaryKey(autoGenerate = true)
+    private int characterId;
+
+//    @ColumnInfo(name = "download_date")
+//    private Date downloadDate;
+//
+//    @ColumnInfo(name = "character_name")
+//    private String name;
+//
+//    @ColumnInfo(name = "image_url")
+//    private String imageUrl;
+//
+//    @ColumnInfo(name = "comics_available")
+//    private int comicsAvail;
+//
+//    @ColumnInfo(name = "characters_type")
+//    private String type;
+//
+//    @ColumnInfo(name = "characters_url")
+//    private String charactersUrl;
+}
