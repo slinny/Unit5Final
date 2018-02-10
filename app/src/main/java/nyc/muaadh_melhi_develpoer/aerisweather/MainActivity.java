11cdbf9a29101641ab47961f4d89e2070df1d7cf
Daniel Cortes <danielcortes41@gmail.com>
2018-02-10 15:30:55 -0500

    update

--- app/src/main/java/nyc/muaadh_melhi_develpoer/aerisweather/MainActivity.java
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/aerisweather/MainActivity.java
@@ -2,15 +2,14 @@ package nyc.muaadh_melhi_develpoer.aerisweather;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
-import static nyc.muaadh_melhi_develpoer.aerisweather.R.id.activity_main_xml;
+
 
 public class MainActivity extends Activity {
 
-    View img;
+    Toolbar img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +17,7 @@ public class MainActivity extends Activity {
 
         // Load the ImageView that will host the animation and
         // set its background to our AnimationDrawable XML resource.
-        img = (View)findViewById(activity_main_xml);
+        img =  (Toolbar) findViewById(R.id.tool_bar);
         img.setBackgroundResource(R.drawable.weather_animation);
 
         // Get the background, which has been compiled to an AnimationDrawable object.
